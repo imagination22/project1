77f1c8022c1df90b28d41cc62b4511cfd0619cd3
Shubham Barman <barmanshubham1822@gmail.com>
2020-07-30 22:53:32 +0530

    Initial commit

---
+++ Participant.py
@@ -0,0 +1,26 @@
+class Coder:
+    def __init__(self, name, rollno, role):
+        self.name = name
+        self.rollno = rollno
+        self.role = role
+
+    def guide(self):
+        print("i know pretty much manny things")
+
+    def frontend(self):
+        if self.role == "frontend":
+            print("i am responsible for ui,ux and flutter work")
+        else:
+            print("there is a mistake in my role")
+
+    def backend(self):
+        pass
+
+
+if __name__ == "__main__":
+    student1 = Coder("Shubham", 50, "frontend")
+    student2 = Coder("yash", 50, "backend")
+    student3 = Coder("Shubham", 50, "frontend")
+    student4 = Coder("Shubham", 50, "frontend")
+    student1.frontend()
+
